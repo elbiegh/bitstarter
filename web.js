fe82a57d8697ee096559b6babb492ee30dc1d714
Bert Barbe <elbiegh@mbox.be>
2013-07-10 18:56:45 +0000

    Read from file

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs=require('fs');
+var docbuf=fs.readFileSync("/home/ubuntu/bitstarter/index.html");
+var docs = docbuf.toString('utf8');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 3!');
+  response.send(doc);
 });
 
 var port = process.env.PORT || 5000;
